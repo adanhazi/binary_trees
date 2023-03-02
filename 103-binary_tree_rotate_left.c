517d6229c3a73c303b285338e72326727b7be72c
adanhazi <110994904+adanhazi@users.noreply.github.com>
2023-03-02 22:10:57 +0300

    Update 103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,40 +1,26 @@
-/* 22. Rotate left */
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - left child of root becomes new root, tree rotated
- * so it retains BST ordering of values (in-order traversal of leaves is same)
- * @tree: tree to left rotate
- * Return: pointer to new root node, or NULL if `root` is NULL
+ * binary_tree_rotate_left - Function that  rotates left the binary tree.
+ * @tree: The root node of the three
+ * Return: Pointer node of the new node
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *pivot;
 
-	if (!tree)
-		return (NULL);
-
-	/* pivot will become new root */
-	pivot = tree->right;
-
-	/* migrate children to keep BST order */
-	tree->right = pivot->left;
-	if (pivot->left)
-		pivot->left->parent = tree;
-
-	/* handle upstream connections if `tree` is a subtree */
-	pivot->parent = tree->parent;
-	if (tree->parent)
+	if (tree == NULL || tree->right == NULL)
 	{
-		if (tree == tree->parent->left)
-			tree->parent->left = pivot;
-		else
-			tree->parent->right = pivot;
+		return (NULL);
 	}
-
-	/* finally rotate pivot into root postion */
+	pivot = tree->right;
+	tree->right = pivot->left;
+	if (pivot->left != NULL)
+	{
+		pivot->left->parent = tree;
+	}
 	pivot->left = tree;
+	pivot->parent = tree->parent;
 	tree->parent = pivot;
-
 	return (pivot);
 }
